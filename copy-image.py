7addcd3924ba4bbff3c9c148071e5590b0b898a4
Cedric Porter <cedricporter@gmail.com>
2013-01-26 21:20:14 +0800

    first commit

---
+++ copy-image.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Hua Liang[Stupid ET] <et@everet.org>
+#
+import os
+import shutil
+
+
+dir = '.'
+output_dir = 'output'
+trash = '/tmp/image-trash'
+MIN_SIZE = 30000                # 30KB
+
+
+def prepare():
+    try:
+        os.mkdir(output_dir)
+    except:
+        pass
+
+
+def filesize_filter(filename):
+    try: os.mkdir(trash)
+    except: pass
+    size = os.path.getsize(filename)
+    if size < MIN_SIZE:
+        shutil.move(filename, trash)
+
+
+def filter():
+    for f in os.listdir(output_dir):
+        filesize_filter(os.path.normpath(output_dir + os.sep + f))
+
+
+def main():
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            if file.endswith('.jpg'):
+                prefix = root[:root.find('-')]  # take the part before '-'
+                fullname = os.path.join(root, file)
+                print fullname
+                outfilename = output_dir + os.sep + prefix + file
+                print outfilename
+                outfilename = os.path.normpath(outfilename)
+                print outfilename
+                print '-' * 30
+
+                shutil.copy(fullname, outfilename)
+            else:
+                print file
+
+
+if __name__ == '__main__':
+    prepare()
+    main()
+    filter()
